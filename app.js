7119065681c034ecb64481d0054c8fe2b9eb4834
ViniciusBorgesCptbr <viniciusborgesbrpt@gmail.com>
2024-06-06 15:58:26 -0700

    rest of links and features

--- app.js
+++ app.js
@@ -17,24 +17,24 @@ function closeMenu() {
   navLinks.style.right = "-100%";
 }
 
-// Select the container where the stars will be appended
-let reviewContainer = document.querySelectorAll(".reviewStars");
+// // Select the container where the stars will be appended
+// let reviewContainer = document.querySelectorAll(".reviewStars");
 
-// Create a new array of length 5 and fill it with undefined values
-Array(5)
-  .fill()
-  .forEach(() => {
-    // Create a new img element
-    let star = document.createElement("img");
+// // Create a new array of length 5 and fill it with undefined values
+// Array(5)
+//   .fill()
+//   .forEach(() => {
+//     // Create a new img element
+//     let star = document.createElement("img");
 
-    // Set the src attribute to the star image path
-    star.src = "img/Star.svg";
+//     // Set the src attribute to the star image path
+//     star.src = "img/Star.svg";
 
-    // Append the star image to the review container
-    reviewContainer.forEach((container) => {
-      container.appendChild(star.cloneNode(true));
-    });
-  });
+//     // Append the star image to the review container
+//     reviewContainer.forEach((container) => {
+//       container.appendChild(star.cloneNode(true));
+//     });
+//   });
 
 // prevent form default action
 
@@ -42,3 +42,8 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 });
+
+// deleting the dom element
+const scrollTop = () => {
+  sectionHero.scrollIntoView({ behavior: "smooth" });
+};
